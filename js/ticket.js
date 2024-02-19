bf308ccde0301944b357274861bd45a136cc8045
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2024-02-19 21:34:18 +0600

    partly completed. not so well in js functions

---
+++ js/ticket.js
@@ -0,0 +1,106 @@
+
+
+let coaches = document.getElementsByClassName('coach');
+
+
+
+
+
+for (const coach of coaches) {
+
+    coach.addEventListener('click', function () {
+
+    coach.setAttribute('disabled', true);
+
+        //selected ticket details
+        coach.style.backgroundColor = '#1dd100';
+        const listUl = document.getElementById('ticket-list');
+        const li = document.createElement('li');
+        li.innerText = coach.innerText;
+        listUl.appendChild(li)
+       
+        const classes = document.getElementById('class-of-bus')
+        const p = document.createElement('p');
+        p.innerText = 'Economy'
+        classes.appendChild(p);
+
+        const prices = document.getElementById('ticket-price');
+        const price = document.createElement('p');
+        price.innerText = '550';
+        prices.appendChild(price)
+
+    //count total ticket
+      const tickets =  document.getElementById('booking-seat-count');
+      const currentTicket = parseInt(tickets.innerText);
+      const updateCount = currentTicket+1;
+      tickets.innerText = updateCount;
+
+    //   total ticket price
+    const ticketPrices = document.getElementById('ticketPrice');
+    const updatePrice = updateCount*550;
+     ticketPrices.innerText = updatePrice + ' BDT';
+    
+     //grand total price
+     const discountPrice = document.getElementById('grand-price');
+     discountPrice.innerText = updatePrice + ' BDT';
+
+     // count total seat left
+      const seats = document.getElementById('seat-left')
+      const seat = parseInt(seats.innerText);
+      const updateSeat = seat - 1;
+      seats.innerText = updateSeat;
+        
+      //
+    
+    })
+   
+}
+
+document.getElementById('input-cupon').addEventListener('keyup', function (event) {
+    const inputCupon = event.target.value;
+    const applyBtn = document.getElementById('btn-cupon-apply');
+    if (inputCupon === 'NEW15' || inputCupon === 'COUPLE20') {
+        applyBtn.removeAttribute('disabled',true);
+    } 
+    else {
+    applyBtn.setAttribute('disabled', true);    
+    }
+})
+
+// document.getElementById('next-btn').addEventListener('keyup', function () {
+//     const numberInput = event.target.value;
+//     const seatCount = event.target.value;
+
+
+
+// })
+   
+
+
+
+
+
+
+
+
+
+
+//=======================================================================================================//
+
+                                         // All test scripts//
+
+
+
+
+// if (updatePrice > 2200) {
+             
+        
+
+//     const discountPrice = document.getElementById('grand-price');
+//          const grandPrice = updatePrice*0.15;
+//        const grandTotalPrice =  updatePrice - grandPrice; 
+//         discountPrice.innerText = grandTotalPrice + ' BDT';
+//  } 
+//  else {
+//     discountPrice.innerText = updatePrice + ' BDT';
+//  }
